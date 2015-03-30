09d2b2e44dc63bde399555b01eb303de77b88702
codyorick <codyorick@gmail.com>
2015-03-30 18:13:24 -0400

    finished assignment 4

---
+++ public/js/app.js
@@ -0,0 +1,66 @@
+﻿var app = angular.module("OnlineUniversity", []);
+
+app.controller("OnlineUniversityController", function ($scope, $http) {
+
+    // initial load of all courses
+    $http.get("/api/courses")
+    .success(function(response) {
+        $scope.courses = response;
+    });
+
+    // delete a course
+    $scope.deleteCourse = function (index) {
+        bootbox.confirm({
+            title: "Remove Course",
+            message: "Remove this course?",
+            callback: function (result) {
+                if (result) {
+                    $http.delete('/api/course/' + index)
+                    .success(function (response) {
+                        $scope.courses = response;
+                    });
+                }
+            }
+        });
+    };
+
+    // show modal when add course button is clicked
+    $scope.addCourse = function () {
+        $scope.add = true;
+        $scope.newCourse = { name: "", category: "", dateCreated: new Date(), description: "" }
+        $scope.selectedcourseindex = null;
+        $("#add-modal").modal();
+    };
+
+    // show modal for editing
+    $scope.editCourse = function (course, index) {
+        $scope.add = false;
+        course.dateCreated = new Date(course.dateCreated);
+        $scope.newCourse = course;
+        $scope.selectedcourseindex = index;
+        $("#add-modal").modal();
+    };
+
+    // save edited course
+    $scope.saveCourse = function (course) {
+        if ($scope.add) {
+            $http.post("/api/course/", course)
+            .success(function (response) {
+                $scope.courses = response;
+            });
+        } else {
+            $http.put("/api/course/" + $scope.selectedcourseindex, course)
+            .success(function (response) {
+                $scope.courses = response;
+            });
+        }
+
+        $("#add-modal").modal('hide');
+    }
+
+    $scope.formatDate = function (date) {
+        var formatteddate = new Date(date).toISOString().substring(0, 10);
+        return formatteddate;
+    }
+
+});
--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
 
-app.get('/', function (req, res) {
-    res.send('Hello!')
-});
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var courses = [
+    { name: "Java 101", category: "PROG", dateCreated: new Date("2015-01-01T18:06:47-04:00"), description: "Wow" },
+    { name: "MongoDB 101", category: "DB", dateCreated: new Date("2015-02-01T18:06:47-04:00"), description: "Good" },
+    { name: "Express 101", category: "PROG", dateCreated: new Date("2015-03-01T18:06:47-04:00"), description: "Better" },
+    { name: "AngularJS 101", category: "WEB", dateCreated: new Date("2015-04-01T18:06:47-04:00"), description: "Best" },
+    { name: "NodeJS 101", category: "PROG", dateCreated: new Date("2015-05-01T18:06:47-04:00"), description: "Awesome" }
+];
+
+// gets all courses
+app.get('/api/courses', function (req, res) {
+    res.json(courses);
+});
+
+// gets course at index (used when editing)
+app.get('/api/course/:index', function (req, res) {
+    var index = req.params.index;
+    res.json(courses[index]);
+});
+
+// delete course at index
+app.delete('/api/course/:index', function (req, res) {
+    var index = req.params.index;
+    courses.splice(index, 1);
+    res.json(courses);
+});
+
+// adds course
+app.post('/api/course/', function (req, res) {
+    var course = req.body;
+    courses.push(course);
+    res.json(courses);
+});
+
+// edit course
+app.put('/api/course/:index', function (req, res) {
+    var index = req.params.index;
+    var updated = req.body;
+    courses[index] = updated;
+    res.json(courses);
+});
 
 app.listen(port, ip)
